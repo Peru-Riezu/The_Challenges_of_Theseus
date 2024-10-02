29fde5201742ed222bce6321fc6a7da433433c54
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-02 09:16:27 +0200

    improvements

--- basque/home/labirintoaren_erdigunea/.bashrc
+++ basque/home/labirintoaren_erdigunea/.bashrc
@@ -113,3 +113,4 @@ if ! shopt -oq posix; then
 fi
 
 cd
+cat helburua
--- basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
+++ basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
@@ -1,54 +1,22 @@
 PARENT_DIR="/home/labirintoaren_erdigunea"            # Parent directory to watch
 TARGET_DIR="sarraila"                 # Directory that the student will create
 KEY_FILE="giltza"                     # The file to watch for inside TARGET_DIR
-EXPECTED_CONTENT="cat fitxategi barruan zaude
-fitxategiak dira bost
-bere barruak ikusteko
-bost cat egitea
-ez zen azkarrena izango
-
-
-Cat egiten duzunean, > erabili, fitxategia sortzeko.
-Bide ikusezin bat, helmugara iristeko.
-Giltzak sortuko dituzu, altxorra aurkitzeko.
-
-
-ginkana hasia da
-eta zu zaude hemen
-izango al zara zu
-altxorretara lehen?
-
-
-Giltza nahi baduzu,
-Fitxategi guztiak batu.
-'giltza' izena eman,
-Eta sarrailan sartu segidan.
-
-giltz bat sarrailik gabe
-ez al du sorginak
-'sarraila' sortzeko botere?
-
-
-man man
-man cat
-man ls
-man mkdir
-cat *
-cat < in > out
-cat in > out
-https://www.gnu.org/software/bash/manual/bash.html
-https://www.gnu.org/software/bash/manual/bash.html#Redirections
-https://www.gnu.org/software/bash/manual/bash.html#Pattern-Matching"     # Replace with the expected content
+EXPECTED_CONTENT=$(cat /home/labirintoaren_erdigunea/*)     # Replace with the expected content
 
 while true; do
   if [[ -d "$PARENT_DIR/$TARGET_DIR" ]]; then
     if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
       FILE_CONTENT=$(cat "$PARENT_DIR/$TARGET_DIR/$KEY_FILE")
       if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
-	clear
-        printf "\n\n\n\n\n\n\n\n\n\n\n\n\nCongratulations! You have solved the riddle.\n\n\n\n\n\n\n\n\n\n\n\n\n" | tee /home/ginkana/sarraila/hurrengo_urratsa
-	read -n1
-        exit 0
+		clear
+        printf "\n\n\n\n\n\n\n\n\n\n\n\n\n\033[32m Zorionak!! Azmakizun hau gainditu duzu\n" \
+			"hurrengo erronkara nahi baduzu jarraitu\n" \
+			"erabiltzailea: irakurri\n" \
+			"eta\n" \
+			"pasahitza: beti\n" \
+			"erabili beharko dituzu\033[0m\n\n\n\n\n\n\n\n\n\n\n\n\n" | tee /home/labirintoaren_erdigunea/sarraila/haria"
+		read -n1
+		exit 0
       fi
     fi
   fi
