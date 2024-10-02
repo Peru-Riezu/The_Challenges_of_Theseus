bb7baddfe5b5a353edfc82d8283ff57855fbe86e
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-02 18:22:07 +0200

    go

--- basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
+++ basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
@@ -11,6 +11,7 @@ while true; do
 			FILE_CONTENT=$(cat "$PARENT_DIR/$TARGET_DIR/$KEY_FILE")
 			if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
 				tput clear > /dev/tty
+				tput civis > /dev/tty
 				printf "%s\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n" \
 					"Asmakizun hau gainditu duzu" \
 					"hurrengo erronkara nahi baldinba duzu jarraitu" \
@@ -19,11 +20,8 @@ while true; do
 					"pasahitza: beti" \
 					"erabili beharko dituzu" >> /home/labirintoaren_erdigunea/sarraila/haria
 				cat /home/labirintoaren_erdigunea/sarraila/haria > /dev/tty
-				tput civis > /dev/tty
 				read -n1 < /dev/tty
 				pkill -SIGUSR1 bash
-				read -n1 < /dev/tty
-				tput cnorm > /dev/tty
 				exit 0
 			fi
 		fi
