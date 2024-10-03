ad7d12551dc5c92b64a22dde4808991475fd991f
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-03 19:31:07 +0200

    go

--- basque/create_users.bash
+++ basque/create_users.bash
@@ -1,8 +1,7 @@
-declare -A users=
-(
-	["labirintoaren_erdigunea"]="piraten_bizitza_oberena_da"
-	["irakurri"]="beti"
-)
+declare -A users
+
+users["labirintoaren_erdigunea"]="piraten_bizitza_oberena_da"
+users["irakurri"]="beti"
 
 for username in "${!users[@]}"; do
 	password="${users[$username]}"
@@ -10,11 +9,3 @@ for username in "${!users[@]}"; do
 	echo "$username:$password" | chpasswd
 	chsh -s /bin/bash "$username"
 done
-
-#useradd -m labirintoaren_erdigunea
-#echo 'labirintoaren_erdigunea:piraten_bizitza_oberena_da' | chpasswd
-#chsh -s /bin/bash labirintoaren_erdigunea
-#
-#useradd -m irakurri
-#echo 'irakurri:beti' | chpasswd
-#chsh -s /bin/bash labirintoaren_erdigunea
