f2c0ef17fb91b956682ec9d97c851e02f288f3c8
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-04 12:11:46 +0200

    go

--- basque/user_monitoring_scripts/monitor_irakurri.bash
+++ basque/user_monitoring_scripts/monitor_irakurri.bash
@@ -5,25 +5,23 @@ COLOR_GREEN="\033[32m"
 COLOR_RESET="\033[0m"
 
 while true; do
-	if [[ -d "$PARENT_DIR/$TARGET_DIR" ]]; then
-		if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
-			files=($(ls -A))
-			if [[ ${#files[@]} -eq 1 ]]; then
-				tput clear
-				tput civis
-				printf "%s\n%s\n%s\n%s\%s\n\n$COLOR_GREEN%s\n%s$COLOR_RESET\n\n" \
-					"oh ene heroi ausarta" \
-					"ikusi zintudanetik dakit" \
-					"sortzailea bezain adimentsua zarela" \
-					"laberinto honetatik egingo duzu ihes" \
-					"berak egin zuen bezala" \
-					"erabiltzailea: irakurri" \
-					"pasahitza: beti" > /home/irakurri/sarraila/haria
-				cat /home/labirintoaren_erdigunea/sarraila/haria
-				read -n1
-				pkill -SIGUSR1 bash
-				exit 0
-			fi
+	if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
+		files=($(ls -A $PARENT_DIR/$TARGET_DIR))
+		if [[ ${#files[@]} -eq 1 ]]; then
+			tput clear
+			tput civis
+			printf "%s\n%s\n%s\n%s\%s\n\n$COLOR_GREEN%s\n%s$COLOR_RESET\n\n" \
+				"oh ene heroi ausarta" \
+				"ikusi zintudanetik dakit" \
+				"sortzailea bezain adimentsua zarela" \
+				"laberinto honetatik egingo duzu ihes" \
+				"berak egin zuen bezala" \
+				"erabiltzailea: irakurri" \
+				"pasahitza: beti" > /home/irakurri/sarraila/haria
+			cat /home/labirintoaren_erdigunea/sarraila/haria
+			read -n1
+			pkill -SIGUSR1 bash
+			exit 0
 		fi
 	fi
 	sleep 0.2
--- basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
+++ basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
@@ -6,24 +6,22 @@ COLOR_GREEN="\033[32m"
 COLOR_RESET="\033[0m"
 
 while true; do
-	if [[ -d "$PARENT_DIR/$TARGET_DIR" ]]; then
-		if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
-			FILE_CONTENT=$(cat "$PARENT_DIR/$TARGET_DIR/$KEY_FILE")
-			if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
-				tput clear
-				tput civis
-				printf "%s\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n" \
-					"Asmakizun hau gainditu duzu" \
-					"hurrengo erronkara nahi baldinba duzu jarraitu" \
-					"erabiltzailea: irakurri" \
-					"eta" \
-					"pasahitza: beti" \
-					"erabili beharko dituzu" > /home/labirintoaren_erdigunea/sarraila/haria
-				cat /home/labirintoaren_erdigunea/sarraila/haria
-				read -n1
-				pkill -SIGUSR1 bash
-				exit 0
-			fi
+	if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
+		FILE_CONTENT=$(cat "$PARENT_DIR/$TARGET_DIR/$KEY_FILE")
+		if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
+			tput clear
+			tput civis
+			printf "%s\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n" \
+				"Asmakizun hau gainditu duzu" \
+				"hurrengo erronkara nahi baldinba duzu jarraitu" \
+				"erabiltzailea: irakurri" \
+				"eta" \
+				"pasahitza: beti" \
+				"erabili beharko dituzu" > /home/labirintoaren_erdigunea/sarraila/haria
+			cat /home/labirintoaren_erdigunea/sarraila/haria
+			read -n1
+			pkill -SIGUSR1 bash
+			exit 0
 		fi
 	fi
 	sleep 0.2
