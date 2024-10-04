8a1e5d59cc8b0110b3c3556904aab720ebff880b
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-04 11:57:04 +0200

    push

--- basque/create_users.bash
+++ basque/create_users.bash
@@ -9,6 +9,7 @@ for username in "${!users[@]}"; do
 	password="${users[$username]}"
 	useradd -m "$username"
 	usermod -aG labirinto_gela "$username"
+	sudo -u "$username" bash -c 'shopt -s extglob'
 	echo "$username:$password" | chpasswd
 	chsh -s /bin/bash "$username"
 done
