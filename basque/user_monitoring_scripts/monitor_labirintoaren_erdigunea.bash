4fd3d2a68da0e75aba7cd62bf79a5ae81721279a
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-06 20:31:56 +0200

    go

--- basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
+++ basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
@@ -11,6 +11,7 @@ while true; do
 		if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
 			old_stty=$(stty -F /dev/tty -g)
 			stty -F /dev/tty -isig -icanon -ixoff -echo min 0 time 0
+			exec 0</dev/null
 			tput clear > /dev/tty
 			tput civis > /dev/tty
 			printf "%s\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n" \
@@ -21,8 +22,9 @@ while true; do
 				"pasahitza: beti" \
 				"erabili beharko dituzu" > /home/labirintoaren_erdigunea/sarraila/haria
 			cat /home/labirintoaren_erdigunea/sarraila/haria
-			stty -F /dev/tty "$old_stty"
+			exec 0</dev/tty
 			read -n1 < /dev/tty
+			stty -F /dev/tty "$old_stty"
 			users_in_group=$(getent group labirinto_gela | awk -F: '{print $4}')
 			IFS=',' read -ra users <<< "$users_in_group"
 			for user in "${users[@]}"; do
