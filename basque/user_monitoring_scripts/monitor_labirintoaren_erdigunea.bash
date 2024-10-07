cb8eead8b458527579624a043a929b9896dc8aa7
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-07 20:47:45 +0200

    go

--- basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
+++ basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
@@ -22,8 +22,7 @@ while true; do
 				"pasahitza: beti" \
 				"erabili beharko dituzu" > /home/labirintoaren_erdigunea/sarraila/haria
 			cat /home/labirintoaren_erdigunea/sarraila/haria > /dev/tty
-			stty -F /dev/tty sane &> /dev/null
-			stty -F /dev/tty -isig -icanon -ixoff -echo &> /dev/null
+			stty -F /dev/tty -igncr
 			dd bs=1 count=1 &>/dev/null
 			stty -F /dev/tty sane &> /dev/null
 			pkill -SIGUSR1 bash
