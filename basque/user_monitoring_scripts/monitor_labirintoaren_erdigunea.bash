8e403ebab92edd9f38c429b20e670fe7d6eeca8a
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-08 20:11:46 +0200

    go

--- basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
+++ basque/user_monitoring_scripts/monitor_labirintoaren_erdigunea.bash
@@ -11,7 +11,7 @@ while true; do
 	if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
 		FILE_CONTENT=$(cat "$PARENT_DIR/$TARGET_DIR/$KEY_FILE")
 		if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
-			stty -F /dev/tty igncr -extproc -isig -icanon -ixoff -echo
+			stty igncr -extproc -isig -icanon -ixoff -echo
 			tput civis > /dev/tty
 			tput clear > /dev/tty
 			printf "%s\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n$COLOR_GREEN%s$COLOR_RESET\n%s\n" \
@@ -23,6 +23,7 @@ while true; do
 				"erabili beharko dituzu" > /home/labirintoaren_erdigunea/sarraila/haria
 			cat /home/labirintoaren_erdigunea/sarraila/haria > /dev/tty
 			stty -F /dev/tty -igncr -extproc -isig -icanon -ixoff -echo
+			tput civis > /dev/tty
 			pkill -SIGUSR1 bash
 			exit 0
 		fi
