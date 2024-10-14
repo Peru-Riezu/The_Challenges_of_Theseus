5f6ed5b13ecdce090ccf43aa020cf600d5600fa8
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-14 21:46:13 +0200

    go

--- basque/user_monitoring_scripts/monitor_git.bash
+++ basque/user_monitoring_scripts/monitor_git.bash
@@ -1,8 +1,8 @@
 PARENT_DIR="/home/git"
 TARGET_DIR="sarrailagile"
 KEY_FILE="aldaketa_fitxategia"
-REPO_PATH=/home/git/repo
-COMMIT_HASH=$(git -C "$REPO_PATH" log --grep="^prest!!$" --pretty=format:"%H" -1)
+REPO_PATH="/home/git/repo"
+COMMIT_HASH=$(git -C "$REPO_PATH" log --grep='^prest!!$' --pretty=format:"%H" -1)
 EXPECTED_CONTENT=$(git -C "$REPO_PATH" show "$COMMIT_HASH":giltza)
 ORIGINAL_CONTENT=$(cat "$REPO_PATH/giltza")
 
