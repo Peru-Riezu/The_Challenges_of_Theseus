fce826bbdb207cbf622211b1590ffd764ba88731
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-22 20:48:49 +0200

    go

--- basque/user_monitoring_scripts/monitor_Bombe.bash
+++ basque/user_monitoring_scripts/monitor_Bombe.bash
@@ -1,15 +1,16 @@
 PARENT_DIR="/home/Bombe"
 TARGET_DIR="sarraila"
-KEY_FILE="giltza"
-TARGET_DIR="sekretu_kutxa"
-KEY_FILE="mezu_enkriptatua.bin"
+KEY_FILE="giltza.pem"
+TARGET_DIR2="sekretu_kutxa"
+KEY_FILE2="mezu_enkriptatua.bin"
 EXPECTED_CONTENT=$(cat /home/Bombe/jatorrizko_mezua)
 
 trap '' SIGINT
 
 while true; do
 	if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" && -f "$PARENT_DIR/$TARGET_DIR2/$KEY_FILE2" ]]; then
-		FILE_CONTENT=$(openssl pkeyutl -decrypt -inkey $PARENT_DIR/$TARGET_DIR/$KEY_FILE -in $PARENT_DIR/$TARGET_DIR2/$KEY_FILE2)
+		FILE_CONTENT=$(openssl pkeyutl -decrypt -inkey $PARENT_DIR/$TARGET_DIR/$KEY_FILE -in \
+			$PARENT_DIR/$TARGET_DIR2/$KEY_FILE2)
 		if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
 			cat /root/basque/user_monitoring_scripts/Bombe_handle_success.bash > /handle_sigint.bash
 			exec 42>/root/lock
