9a724c8e4ac1fdb3d810497c924ab22d2ed099ee
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-22 21:42:11 +0200

    go

--- basque/launch_monitors.bash
+++ basque/launch_monitors.bash
@@ -5,3 +5,4 @@ bash /root/basque/user_monitoring_scripts/monitor_madarikatua.bash &
 bash /root/basque/user_monitoring_scripts/monitor_git.bash &
 bash /root/basque/user_monitoring_scripts/monitor_eragile_analitikoa.bash &
 bash /root/basque/user_monitoring_scripts/monitor_Z3.bash &
+bash /root/basque/user_monitoring_scripts/monitor_Bombe.bash &
