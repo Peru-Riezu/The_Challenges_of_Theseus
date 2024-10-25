4e8fde4779fd1ade90c2754cfc72684558a13a1e
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-10-25 09:40:21 +0200

    go

--- sshd_files/launch_container.bash
+++ sshd_files/launch_container.bash
@@ -1,4 +1,5 @@
-bash /etc/ssh/reroute_all_ips.bash
+bash /etc/ssh/reroute_all_ips.bash &> /dev/null
+
 docker run --net=host -h The_Challenges_of_Theseus --rm -it --storage-opt size=1G --cpus=0.25 --memory=200m \
 	--user root the_challenges_of_theseus_container bash -c \
 	"bash /root/launch_all_monitors.bash && bash /root/update_all_hosts.bash && exec gosu $USER bash -i"
