62fd1dc6e6f9c29a0dbef33b58ad22816adbb302
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-11-08 14:26:42 +0100

    go

--- basque/reroute_ips.bash
+++ basque/reroute_ips.bash
@@ -1,6 +1,6 @@
 grep -q "^net.ipv4.ip_forward = 1$" /etc/sysctl.conf || echo "net.ipv4.ip_forward = 1" | tee -a /etc/sysctl.conf
-sysctl -p > /dev/null
+/usr/sbin/sysctl -p > /dev/null
 
-iptables -C FORWARD -d 127.0.0.2 -j ACCEPT 2>/dev/null || iptables -A FORWARD -d 127.0.0.2 -j ACCEPT
-iptables -t nat -C OUTPUT -d 9.0.0.1 -j DNAT --to-destination 127.0.0.2 2>/dev/null || \
-iptables -t nat -A OUTPUT -d 9.0.0.1 -j DNAT --to-destination 127.0.0.2
+/usr/sbin/iptables -C FORWARD -d 127.0.0.2 -j ACCEPT 2>/dev/null || iptables -A FORWARD -d 127.0.0.2 -j ACCEPT
+/usr/sbin/iptables -t nat -C OUTPUT -d 9.0.0.1 -j DNAT --to-destination 127.0.0.2 2>/dev/null || \
+/usr/sbin/iptables -t nat -A OUTPUT -d 9.0.0.1 -j DNAT --to-destination 127.0.0.2
