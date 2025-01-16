51fc7b0d6c4925308a4fc8cfc05c382578cabc45
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2025-01-16 11:29:59 +0100

    go

--- common_functions/monitors_lock.bash
+++ common_functions/monitors_lock.bash
@@ -6,18 +6,12 @@ get_root_lock()
 
 handle_succes_and_release_lock()
 {
-	for pid in $(pgrep bash); do
-		prlimit --pid $pid --nofile=201:201
-	done
 	pkill -SIGINT bash
 	while [ ! -f "/user_shell_files/shells_working" ]; do
 		sleep 0.01
 	done
 	while [ -f "/user_shell_files/shells_working" ]; do
 		sleep 0.1
-	done
-	for pid in $(pgrep bash); do
-		prlimit --pid $pid --nofile=50:50
 	done
 	echo "" > /handle_sigint.bash
 	flock -u 42
