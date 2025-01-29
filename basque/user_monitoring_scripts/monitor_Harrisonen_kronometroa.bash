478ac478acff376acd2bd80cde4b6d578cffa2ab
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2025-01-29 12:02:23 +0100

    go

--- basque/user_monitoring_scripts/monitor_Harrisonen_kronometroa.bash
+++ basque/user_monitoring_scripts/monitor_Harrisonen_kronometroa.bash
@@ -10,7 +10,7 @@ while true; do
 	if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
 		get_root_lock
 		cat /root/common_functions/handle_success.bash > /handle_sigint.bash
-		cat /root/basque/user_monitoring_scripts/git_handle_success.bash >> /handle_sigint.bash
+		cat /root/basque/user_monitoring_scripts/Harrisonen_kronometroa_handle_success.bash >> /handle_sigint.bash
 		handle_succes_and_release_lock
 		exit 0
 	fi
