92b8fc48ae2dd7840910605ff809a073b1ecab53
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2025-02-14 00:39:18 +0100

    go

--- basque/user_monitoring_scripts/monitor_eragile_analitikoa.bash
+++ basque/user_monitoring_scripts/monitor_eragile_analitikoa.bash
@@ -3,14 +3,14 @@ source /root/common_functions/monitors_lock.bash
 PARENT_DIR="/home/eragile_analitikoa"
 TARGET_DIR="sarraila"
 KEY_FILE="giltza"
-EXPECTED_CONTENT=$(find /home/eragile_analitikoa/sustraiak -perm 777 | xargs cat)
+EXPECTED_OUTPUT=$(find /home/eragile_analitikoa/sustraiak -perm 777)
 
 trap '' SIGINT
 
 while true; do
 	if [[ -f "$PARENT_DIR/$TARGET_DIR/$KEY_FILE" ]]; then
-		FILE_CONTENT=$(cat "$PARENT_DIR/$TARGET_DIR/$KEY_FILE")
-		if [[ "$FILE_CONTENT" == "$EXPECTED_CONTENT" ]]; then
+		GOTTEN_CONTENT=$(stat -c '%N' $PARENT_DIR/$TARGET_DIR/$KEY_FILE | awk -F" -> " '{print $2}' | tr -d "'")
+		if [[ "$GOTTEN_CONTENT" == "$EXPECTED_OUTPUT" ]]; then
 			get_root_lock
 			cat /root/common_functions/handle_success.bash > /handle_sigint.bash
 			cat /root/basque/user_monitoring_scripts/eragile_analitikoa_handle_success.bash >> /handle_sigint.bash
